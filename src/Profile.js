fc3ab55a3f5314ee6519286844d8e9ed1e3e21ed
Yusuf Azeez <highshow74@gmail.com>
2022-10-27 17:21:12 +0100

    Added: profile-img to Profile component

--- src/Profile.js
+++ src/Profile.js
@@ -1,8 +1,11 @@
+import profileImg from './img/profile-img.jpg';
 
 export default function Profile() {
   return (
     <section className="profile-section">
-      
+      <div id="profile-img">
+        <img src={profileImg} alt="profile-img" />
+      </div>
     </section>
   )
 }
