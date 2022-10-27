19700ee6866bc43a21f5ba2ff90b13c870e3f378
Yusuf Azeez <highshow74@gmail.com>
2022-10-27 18:14:16 +0100

    Added: different links to each button present in Links component

--- src/Links.js
+++ src/Links.js
@@ -1,8 +1,36 @@
-
 export default function Links() {
   return (
     <div className="links">
-      
+      <a href="https://twitter.com/YussaMoney">
+        <button id="twitter-link">
+          Twitter Link
+        </button>
+      </a><br />
+      <a href="https://training.zuri.team/">
+        <button id="btn-zuri">
+          Zuri Team
+        </button>
+      </a><br />
+      <a href="http://books.zuri.team">
+        <button id="books">
+          Zuri Books
+        </button>
+      </a><br />
+      <a href="https://books.zuri.team/python-for-beginners?ref_id=Yussassiph">
+        <button id="book-python">
+          Python Books
+        </button>
+      </a><br />
+      <a href="https://background.zuri.team">
+        <button id="pitch">
+          Background Check for Coders
+        </button>
+      </a><br />
+      <a href="https://books.zuri.team/design-rules ">
+        <button id="book-design">
+          Design Books
+        </button>
+      </a>
     </div>
   )
 }
