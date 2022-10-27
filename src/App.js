2ede54ed67e397d13bb301c20edaf4abdc6d8e91
Yusuf Azeez <highshow74@gmail.com>
2022-10-27 17:33:03 +0100

    Added: Profile component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import Profile from "./Profile.js";
+
 function App() {
   return (
     <div className="app">
       <main className="app-main">
-        
+        <section className="profile-section">
+          <Profile />
+        </section>
       </main>
     </div>
   );
