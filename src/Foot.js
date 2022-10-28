b7cd1977824e0e079e795bbc51cd0acea9d87b73
Yusuf Azeez <highshow74@gmail.com>
2022-10-28 04:59:09 +0100

    Added: contents to foot component

--- src/Foot.js
+++ src/Foot.js
@@ -1,9 +1,13 @@
-
+import internship from './img/zuri.png'
+import ingressive from './img/zuri-ingressive-img.png'
 
 export default function Foot() {
   return (
     <div className="foot">
-
+    
+      <img src={internship} alt="zuri internship" id="zuri-img" />
+      <p id="hng-text">HNG Internship 9 Frontend Task</p>
+      <img src={ingressive} alt="Inngressive" id="zuri-ingressive" />
     </div>
   )
 }
