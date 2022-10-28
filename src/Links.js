318d9dca0e636e2f5b400e58f13447fbaf478f42
Yusuf Azeez <highshow74@gmail.com>
2022-10-29 00:16:14 +0100

    Removed unneccessary text/code

--- src/Links.js
+++ src/Links.js
@@ -1,35 +1,37 @@
 import slack from './img/slack-icon.jpg'
 import github from './img/github-icon.png'
-
 export default function Links() {
   return (
     <div className="links"><br />
       <a href="https://twitter.com/YussaMoney" id="twitter_link">
         Twitter Link
       </a><br /><br />
+
       <a href="https://training.zuri.team/" id="btn_zuri">
         Zuri Team
       </a><br /><br />
+
       <a href="http://books.zuri.team" id="books">
-        <span>Zuri Books</span>
-        <small>gghshsh</small>
+        Zuri Books
       </a><br /><br />
+
       <a href="https://books.zuri.team/python-for-beginners?ref_id=Yussassiph" id="book_python">
-        <span>Python Books</span>
-        <small>gghshsh</small>
+        Python Books
       </a><br /><br />
+
       <a href="https://background.zuri.team" id="pitch">
-        <span>Background Check for Coders</span>
-        <small>gghshsh</small>
+        Background Check for Coders
       </a><br /><br />
+
       <a href="https://books.zuri.team/design-rules" id="book_design">
-        <span>Design Books</span>
-        <small>gghshsh</small>
+        Design Books
       </a><br /><br />
+
       <div className="social-icon">
-        <img src={slack} alt="slack-icon" id="slack-icon" />
-        <img src={github} alt="github-icon" id="github-icon" />
+        <a href="https://hng9.slack.com/Yussassiph"><img src={slack} alt="slack-icon" id="slack-icon" /></a>
+        <a href="https://github.com/YussaMoney"><img src={github} alt="github-icon" id="github-icon" /></a>
       </div><br /><br /><br />
+      
     </div>
   )
 }
