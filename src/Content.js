e0277cb6a540433b8df65d8e3a02215bbd2e19ae
Yusuf Azeez <highshow74@gmail.com>
2022-11-04 10:40:11 +0100

    Removed unnecessary text/code

--- src/Content.js
+++ src/Content.js
@@ -1,22 +1,15 @@
-// import Contact from './Contact';
-import Foot from './Foot';
 import Links from './Links';
 import Profile from './Profile';
 
 export default function Content() {
   return (
-    <div className="app">
-      <main className="app-main">
-        <section className="profile-section">
-          <Profile />
-        </section>
-        <section className="links-section">
-          <Links />
-        </section>
-        <section className="foot-section">
-          <Foot />
-        </section>
-      </main>
-    </div>
+    <main className="app-main">
+      <section className="profile-section">
+        <Profile />
+      </section>
+      <section className="links-section">
+        <Links />
+      </section>
+    </main>
   );
 }
