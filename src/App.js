4efd96aa59a7ef1adfbe23327e8610ad3adcf488
Yusuf Azeez <highshow74@gmail.com>
2022-11-04 10:41:45 +0100

    feat: Added permanent foot component that shows on the each route path

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import { Routes, Route } from 'react-router-dom';
 import Contact from './Contact';
 import Content from './Content';
+import Foot from './Foot';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/linktree" element={<Content />} />
-      <Route path="/contact" element={<Contact />} />
-    </Routes>
+    <div className="app">
+      <Routes>
+        <Route path="/linktree" element={<Content />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <section className="foot-section">
+        <Foot />
+      </section>
+    </div>
   );
 }
 
