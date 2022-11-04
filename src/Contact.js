3d18d21abfc32503acab93a1b2fe4bb59b3d17cb
Yusuf Azeez <highshow74@gmail.com>
2022-11-04 08:21:57 +0100

    feat: Added contact form with submit button

--- src/Contact.js
+++ src/Contact.js
@@ -1,11 +1,68 @@
 export default function Contact() {
+  const name = 'Yussassiph';
   const contact = {
-    title: 'contact me',
+    title: 'Contact Me',
+    titleFoot: 'Hi there, contact me to ask me about anything you have in mind.',
+    checkMessage: `You agree to providing your data to ${name} who may contact you.`,
   };
 
   return (
-    <div>
-      <h1>{contact.title}</h1>
+    <div className="contact">
+      <h1 className="title">{contact.title}</h1>
+      <p className="titleFoot">{contact.titleFoot}</p>
+
+      <form action="/" method="post">
+        <label htmlFor="first_name" id="first_name">
+          First name
+          <input
+            type="text"
+            id="first_name"
+            placeholder="Enter your first name"
+            required
+          />
+        </label>
+
+        <label htmlFor="last_name" id="last_name">
+          Last name
+          <input
+            type="text"
+            id="last_name"
+            placeholder="Enter your last name"
+            required
+          />
+        </label>
+
+        <label htmlFor="email" id="email">
+          Email
+          <input
+            type="text"
+            id="email"
+            placeholder="yourname@email.com"
+            required
+          />
+        </label>
+
+        <label htmlFor="message" id="message">
+          Message
+          <textarea
+            name="message"
+            id="message"
+            placeholder="Send me a message and I'll reply you as soon as possible..."
+            required
+          />
+        </label>
+
+        <label htmlFor="confirm" id="confirm">
+          <input
+            type="checkbox"
+            name="confirm"
+            id="confirm"
+          />
+          {contact.checkMessage}
+        </label>
+
+        <button type="submit" id="btn__submit">Send message</button>
+      </form>
     </div>
   );
 }
